785f7398432d33fde0b3131339c00db1c76455d6
Amit Baid <baidstar@hotmail.com>
2021-07-28 13:52:19 -0600

    Added connection message and name

--- client.js
+++ client.js
@@ -1,16 +1,20 @@
 const net = require("net");
 // establishes connection to game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
-    host: '135.23.223.123', // IP address here,
-    port: 50542 // PORT number here,
+    host: '135.23.223.133',
+    port: 50542
   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding('utf8');
 
-  conn.on("data", (message) => {
-    console.log(`Server: ${message}`)
+  conn.on('connect', () => {
+    console.log("Connected to SERVER!");
+    conn.write("Name: AB");
+  });
+  
+  conn.on('data', data => {
+    console.log(data);
   });
 
   return conn;
