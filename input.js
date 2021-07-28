9daa697668eb9589615d61e2ec285b88e95ba405
Amit Baid <baidstar@hotmail.com>
2021-07-28 14:28:48 -0600

    created an input file and added ability to move the snake

---
+++ input.js
@@ -0,0 +1,33 @@
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+let connection;
+
+
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === "w" || key === "W") {
+    connection.write("Move: up")
+  }
+  if (key === "a" || key === "A") {
+    connection.write("Move: left")
+  }
+  if (key === "s" || key === "S") {
+    connection.write("Move: down")
+  }
+  if (key === "d" || key === "D") {
+    connection.write("Move: right")
+  }
+};
+
+module.exports = { setupInput }
